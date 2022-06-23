cd4ffb9bbbbaac42fb6b5508edf8cebf6c500c97
SerhiiMyk <sergey.mykolaichuk@gmail.com>
2022-06-23 09:23:18 +0300

    create contacts-page, contact-details-page, add-contact-modal components,contacts service,contact interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsPageComponent } from './components/contacts-page/contacts-page.component';
+import { ContactDetailsPageComponent } from './components/contact-details-page/contact-details-page.component';
+import { AddContactModalComponent } from './components/add-contact-modal/add-contact-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsPageComponent,
+    ContactDetailsPageComponent,
+    AddContactModalComponent
   ],
   imports: [
     BrowserModule,
